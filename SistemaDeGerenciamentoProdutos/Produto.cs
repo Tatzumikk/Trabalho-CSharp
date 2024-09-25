06691c3d40aacc580995aa286e0bea1fe84aa71e
Vitor P Moreira <vipmoreira@hotmail.com>
2024-09-24 23:05:06 -0300

    Update classe Produto.cs

---
+++ SistemaDeGerenciamentoProdutos/Produto.cs
@@ -0,0 +1,8 @@
+public class Produto
+{
+    public int ID { get; set; }
+    public string? NomeProduto { get; set; }
+    public string? CodigoProduto { get; set; } 
+    public decimal Preco { get; set; }
+    
+}
