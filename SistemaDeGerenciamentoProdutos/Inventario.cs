2e62df809447757fb3ef3e8f92e52a69d0002c03
mariana-kleina <102862145+mariana-kleina@users.noreply.github.com>
2024-09-24 22:52:26 -0300

    Update Inventario.cs

---
+++ SistemaDeGerenciamentoProdutos/Inventario.cs
@@ -0,0 +1,7 @@
+public class Inventario
+{
+    public int ID { get; set; }
+    public string? NomeProduto { get; set; }  
+    public string? Descricao { get; set; }     
+    public int Quantidade { get; set; }        
+}
